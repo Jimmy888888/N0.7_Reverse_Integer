012f7a8ef8604c57a8e88a50a112d57a6b784bcd
康任均 <kangrenjun@kangrenjundeMacBook-Air.local>
2022-07-01 00:12:14 +0800

    2022/07/01 update01 No.7 js

--- N0.7_Reverse_Integer.js
+++ N0.7_Reverse_Integer.js
@@ -2,7 +2,45 @@
  * @param {number} x
  * @return {number}
  */
-//loop(){ tx = x % 10; rx = rx * 10 + tx; x = Math.floor(x / 10);}
+/*
+pseudo code
+let zero_tail = true;
+loop(x>0){ tx = x % 10; zero_tail=( tx != 0 ? false: true); rx = rx * 10 + tx; x = Math.floor(x / 10);}
+*/
  var reverse = function(x) {
-    
+    let zero_tail = true;
+    let negtive = false;
+    let remainder = 0;
+    let rev_x = 0;
+    //avoid js overflow
+    if( x < 0)
+    {
+        x = x * -1;
+        negtive = true;
+    }
+    while( x > 0)
+    {
+        remainder = x % 10;
+        console.log(remainder);
+      
+        rev_x = rev_x * 10 + remainder;
+        
+        x = Math.floor(x / 10);
+    }
+    rev_x = ( negtive ? rev_x * -1 : rev_x);
+    //rev_x's range [-231, 231 - 1]
+    if (rev_x < Math.pow(-2, 31) || rev_x > Math.pow(2, 31) - 1)
+    {
+        return 0;
+    } 
+    return rev_x;
 };
+
+
+/*
+let t = -15;
+console.log( t % 10);//output: -5
+*/
+
+let x = 220300;
+console.log( reverse(x));
